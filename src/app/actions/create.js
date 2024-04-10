a2f3f4d5cc760f6d9896a39d02fa55dc399ffb89
ihossen016 <ihossen016@gmail.com>
2024-04-10 13:23:57 +0600

    save data in redis

--- src/app/actions/create.js
+++ src/app/actions/create.js
@@ -1,10 +1,23 @@
-'use server'
+"use server";
 
-// import { client } from "@/lib/db"
-import { redirect } from 'next/navigation'
+import { client } from "@/lib/db";
+import { redirect } from "next/navigation";
 
 export async function createBook(formData) {
-  const {title, rating, author, blurb} = Object.fromEntries(formData)
+    const { title, rating, author, blurb } = Object.fromEntries(formData);
 
-  
+    // create book id
+    const id = generateId();
+
+    // save new hash for the book
+    await client.hSet(`books:${id}`, {
+        title,
+        rating,
+        author,
+        blurb,
+    });
+}
+
+function generateId() {
+    return Date.now().toString(36) + Math.random().toString(36).slice(2, 11);
 }
--- src/app/create/page.jsx
+++ src/app/create/page.jsx
@@ -1,30 +1,41 @@
-'use client'
+"use client";
 
-import { createBook } from '@/app/actions/create'
-import { useState } from "react"
+import { createBook } from "@/app/actions/create";
+import { useState } from "react";
+import { redirect } from "next/navigation";
 
 export default function Create() {
-  const [error, setError] = useState('')
+    const [error, setError] = useState("");
 
-  async function handleSubmit(formData) {
-    const result = await createBook(formData)
+    async function handleSubmit(formData) {
+        const result = await createBook(formData);
 
-    if (result?.error) {
-      setError(result.error)
+        if (result?.error) {
+            setError(result.error);
+        } else {
+            redirect("/");
+        }
     }
-  }
 
-  return (
-    <main>
-      <form action={handleSubmit}>
-        <h2>Add a New Book</h2>
-        <input type="text" name="title" placeholder="title" />
-        <input type="text" name="author" placeholder="author" />
-        <input type="number" name="rating" max={10} min={1} placeholder="rating" />
-        <textarea name="blurb" placeholder="blurb..."></textarea>
-        <button type="submit" className="btn">Add Book</button>
-        {error && <div className="error">{error}</div>}
-      </form>
-    </main>
-  )
+    return (
+        <main>
+            <form action={handleSubmit}>
+                <h2>Add a New Book</h2>
+                <input type="text" name="title" placeholder="title" />
+                <input type="text" name="author" placeholder="author" />
+                <input
+                    type="number"
+                    name="rating"
+                    max={10}
+                    min={1}
+                    placeholder="rating"
+                />
+                <textarea name="blurb" placeholder="blurb..."></textarea>
+                <button type="submit" className="btn">
+                    Add Book
+                </button>
+                {error && <div className="error">{error}</div>}
+            </form>
+        </main>
+    );
 }
