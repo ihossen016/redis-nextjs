3cc0bc7886705fd75d0f8a8eaf9f02f58a78a36e
ihossen016 <ihossen016@gmail.com>
2024-04-10 14:10:00 +0600

    get redis data using pipeline

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,19 +1,42 @@
-import Link from 'next/link'
+import { client } from "@/lib/db";
+import Link from "next/link";
 
 const getBooks = async () => {
-  
-}
+    // get all books with their scores
+    const results = await client.zRangeWithScores("books", 0, -1);
+
+    // get hash for each book using pipeling
+    const books = Promise.all(
+        results.map((book) => client.hGetAll(`books:${book.score}`))
+    );
+
+    return books;
+};
 
 export default async function Home() {
+    const books = await getBooks();
 
-  return (
-    <main>
-      <nav className="flex justify-between">
-        <h1 className='font-bold'>Books on Redis!</h1>
-        <Link href="/create" className="btn">Add a new book</Link>
-      </nav>
-      
-      <p>List of books here.</p>
-    </main>
-  )
+    return (
+        <main>
+            <nav className="flex justify-between">
+                <h1 className="font-bold">Books on Redis!</h1>
+                <Link href="/create" className="btn">
+                    Add a new book
+                </Link>
+            </nav>
+
+            {books.length === 0 ? (
+                <p>No books found</p>
+            ) : (
+                books.map((book) => (
+                    <div key={book.title} className="card">
+                        <h2>{book.title}</h2>
+                        <p>By {book.author}</p>
+                        <p>{book.blurb}</p>
+                        <p>Rating: {book.rating}</p>
+                    </div>
+                ))
+            )}
+        </main>
+    );
 }
